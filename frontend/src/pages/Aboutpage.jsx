b4d04993899717f7e8b6eeed14131399b6f8b2ef
CarneAUT <ydq3414@autuni.ac.nz>
2025-08-13 15:26:16 +1200

    AboutPage CSS
    Added a CSS file for about page.

--- frontend/src/pages/Aboutpage.jsx
+++ frontend/src/pages/Aboutpage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import '../css/AboutPage.css';
+
+import { RiArrowDropDownLine } from "react-icons/ri";
+import Gamecard from '../components/Gamecard';
+import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios';
 
 function Aboutpage() {
   return (
-    <div>
+    <div class="main">
       <h1>About the AUT Gamestore</h1>
-      <p>This is a platform for buying and selling games.</p>
+      <p>This is a game store platform primarily for COMP710 game programming students to display the work they have done for the course.</p>
     </div>
   )
 }
