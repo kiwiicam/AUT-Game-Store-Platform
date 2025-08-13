21bbeded1bdf6a26c9fa1d117fe3b66aadb5e26f
kiwiicam <mastercamnz@gmail.com>
2025-08-14 11:53:48 +1200

    lol

--- frontend/src/components/AccountSettingsComponents/ManageUploads.jsx
+++ frontend/src/components/AccountSettingsComponents/ManageUploads.jsx
@@ -6,6 +6,12 @@ function ManageUploads() {
 
     useEffect(() => {
 
+        setUploadRequests(
+            [
+                { account: "Blaine Mcdonald", type: "Team Game Project", date: "August 13, 2025" }
+            ]
+        )
+
     }, []);
 
     return (
@@ -17,7 +23,22 @@ function ManageUploads() {
             <div className="bottom-uploads">
                 <div className="scrollable-uploads">
                     <h3>Requests</h3>
+                    <div className="what"></div>
                     <div className="thin-grey-line"></div>
+                    <div className="scroll-manage-uploads">
+                        {uploadRequests.map((item, index) => (
+                            <>
+                                <div className="upload-item" key={index}>
+                                    <h2>{item.account}</h2>
+                                    <h2>{item.type}</h2>
+                                    <h2>{item.date}</h2>
+                                </div>
+                                <div className="thin-grey-line"></div>
+                            </>
+                        ))
+
+                        }
+                    </div>
                 </div>
             </div>
         </div>
