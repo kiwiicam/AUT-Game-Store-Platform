c8d0c8dda068f2c37883f4a534b46e76df781281
Holbdolb <177274899+CarneAUT@users.noreply.github.com>
2025-10-14 15:51:46 +1300

    About Page Fixes
    Added the text to the about page. Fixed padding height in css.

--- frontend/src/pages/Aboutpage.jsx
+++ frontend/src/pages/Aboutpage.jsx
@@ -9,14 +9,21 @@ import axios from 'axios';
 function Aboutpage() {
   return (
     <div class="main">
-      <h1>About the AUT Gamestore</h1>
-      <p>This is a game store platform primarily for COMP710 game programming students to display the work they have done for the course. We hope that people from all walks of life can come and see the great work they have done and so the people behind the projects can prove their compitence in the field. Publishers can grow confidence in their abilities and get a sort of idea on how they performed by gaining feedback from an array of random users.</p>
-      <img class="APImg" src="https://krikey-ai.ghost.io/content/images/size/w1200/2024/08/screen-shot-2.jpg" alt="" />
-      <p class="FigureText">Picture of someones game.</p>
+      <h1>Purpose of the AUT Gamestore</h1>
+      <p>This website is used to present the workings done by the students of COMP710 Game Programming. Our goal is to properly credit and display not only the authors and their work but their effort, time and tools used while making them. We hope that we may present the culmination of all the students' work done over the semester for future game developers or potential employers to see.</p>
+      
+      {/* If we want to add images of games or something */}
+      {/*<img class="APImg" src="https://krikey-ai.ghost.io/content/images/size/w1200/2024/08/screen-shot-2.jpg" alt="" />
+      <p class="FigureText">Picture of someones game.</p>*/}
+
+      <h1>The Course:</h1>
+      <p>The students are tasked with making a personal portfolio showing their work done in both an individual and group game programming assignment. They learn the theory and practical skill to not only create interactive media but the backend engine that runs it. Throughout the semester they learn key teamwork and management skills while being under a heavy time restraint causing development in their time management. They are taught about the relevant industry and its history, the standards within the industry and the culture that comes with it. The portfolio they make will show their technical and creative growth throughout the course and the outcomes of their individual and team assignments will further prove their capabilities and growth.</p>
+
       <h1>About Us</h1>
-      <p>This is our first real project ever for our capstone in our last year in our degree at AUT. We share empathy with the students whos work could be displayed here and support their journey with us.</p>
-      <img class="APImg" src="https://s3.mordorintelligence.com/online-casual-games-market/online-casual-games-market-Online-Casual-Games-Market-Market-Share-by-Platform--2024-1750513864181.webp? Source: https://www.mordorintelligence.com/industry-reports/online-casual-games-market" alt="" />
-      <p class="FigureText">Example Image.</p>
+      <p>As third year students of computer and information sciences at AUT. We each chose to take this project for our capstone project. Each of us specialise in slightly different roles and reflected as much within the creation of the gamestore. Some of us have experience in the related course giving us more personal motivation when making the gamestore. It has been great experience for our future careers in whatever we do next. - Campbell Boulton, Joshua Knight, Blaine McDonald, Carne Soper, Sian Villeren.</p>
+      {/* If we want to add images of games or something */}
+      {/*<img class="APImg" src="https://s3.mordorintelligence.com/online-casual-games-market/online-casual-games-market-Online-Casual-Games-Market-Market-Share-by-Platform--2024-1750513864181.webp? Source: https://www.mordorintelligence.com/industry-reports/online-casual-games-market" alt="" />
+      <p class="FigureText">Example Image.</p>*/}
     </div>
   )
 }
